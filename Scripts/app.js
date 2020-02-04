fb7e548af4402c2fa42c8549b78239aabcc9b60d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-02-03 19:52:43 -0500

    added 5 pages to site

--- Scripts/app.js
+++ Scripts/app.js
@@ -15,9 +15,55 @@ let app;
      */
     function Start()
     {
+       PageSwitcher();
+
+        
+       
        
 
+
+
+      
+
         Main();
+    }
+
+    function PageSwitcher()
+    {
+        let name = window.location.pathname;
+
+       let pageName = name.substring(1, name.length - 5);
+
+       switch(pageName)
+        {
+            case "index":
+               HomePageContent();
+                break;
+            case "products":
+                break;
+            case "services":
+                break;
+            case "about":
+                break;
+            case "contact":
+                break;
+            case "projects":
+                break;
+            default:
+                console.error("Landed in a page that is not defined");
+                break;
+        }
+
+        // add a class of active to the active link
+        $("#"+pageName).addClass("active");
+    }
+
+    function HomePageContent()
+    {
+        document.getElementById("home").className = "nav-item active";
+        $("button").click(()=>{
+            location.href = "projects.html";
+        });
     }
 
     /**
