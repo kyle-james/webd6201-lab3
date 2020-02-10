56ec01046aca9c51a1d2d1af217061520155d9fa
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-02-10 18:22:28 -0500

    modified project for lesson 6

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,17 @@
+class Contact
+{
+    constructor(contactName = "", emailAddress = "", contactNumber = "", contactMessage = "")
+    {
+        this.contactName = contactName;
+        this.emailAddress = emailAddress;
+        this.contactNumber = contactNumber;
+        this.contactMessage = contactMessage;
+    }
+}
+
+
+
+
 "use strict";
 //IIFE - Immediately Invoked Function Expression
 // mean? -> anonymous self-executing function
@@ -7,7 +21,7 @@ let app;
 
     // Declare Function Variables here...
     console.log("%cDeclaring Variables", "color: red;")
-   
+    let contactObject = new Contact();
 
     /**
      * Variable initialization in this function
@@ -37,17 +51,22 @@ let app;
        switch(pageName)
         {
             case "index":
-               HomePageContent();
+               DisplayHomePageContent();
                 break;
             case "products":
+                DisplayProductsContent();
                 break;
             case "services":
+                DisplayServicesContent();
                 break;
             case "about":
+                DisplayAboutContent();
                 break;
             case "contact":
+                DisplayContactContent();
                 break;
             case "projects":
+                DisplayProjectsContent();
                 break;
             default:
                 console.error("Landed in a page that is not defined");
@@ -58,12 +77,91 @@ let app;
         $("#"+pageName).addClass("active");
     }
 
-    function HomePageContent()
+    function DisplayHomePageContent()
     {
         document.getElementById("home").className = "nav-item active";
         $("button").click(()=>{
             location.href = "projects.html";
         });
+    }
+
+    function DisplayProductsContent()
+    {
+
+    }
+
+    function DisplayServicesContent()
+    {
+
+    }
+
+    function DisplayAboutContent()
+    {
+
+    }
+
+    function DisplayContactContent()
+    {
+        function clearForm()
+        {
+            //document.getElementById("contactForm").reset();
+            $("#contactForm")[0].reset();
+        }
+
+        $("#contactName").change((e)=>
+        {
+            if( $("#contactName").length < 8)
+            {
+                console.log("Contact Name Too Short");
+            }
+            console.log("changed");
+        });
+
+        $("#contactName").focus((e)=>
+        {
+            $("#contactName").select();
+            console.log("focus");
+        });
+
+
+        $("#submitButton").click((e)=>
+        {
+            e.preventDefault();
+            let contactName = $("#contactName").val();
+            let emailAddress = $("#emailAddress").val();
+            let contactNumber = $("#contactNumber").val();
+            let contactMessage = $("#contactMessage").val();
+
+            console.log(`Contact Name: ${contactName}`);
+            console.log(`Email Address: ${emailAddress}`);
+            console.log(`Contact Number: ${contactNumber}`);
+            console.log(`Contact Message: ${contactMessage}`);
+
+            contactObject.contactName = contactName;
+            contactObject.emailAddress = emailAddress;
+            contactObject.contactNumber = contactNumber;
+            contactObject.contactMessage = contactMessage;
+
+            console.log(contactObject);
+
+            clearForm();
+        });
+
+        $("#resetButton").click((e)=>
+        {
+            e.preventDefault();
+            if(confirm("Are You Sure?"))
+            {
+                clearForm();
+            }
+
+            
+        });
+    }
+
+    function DisplayProjectsContent()
+    {
+
     }
 
     /**
